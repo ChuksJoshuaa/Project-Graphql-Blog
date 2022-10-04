7e978eb4d493f9216328a86285af23e576ff07c8
Chuks Joshua <chuksmbanasoj@gmail.com>
2022-10-04 08:15:36 +0100

    Initial Commit

---
+++ components/index.js
@@ -0,0 +1,8 @@
+import Categories from "./Categories";
+import PostCard from "./PostCard";
+import PostWidget from "./PostWidget";
+import Header from "./Header";
+import Layout from "./Layout";
+
+
+export {Categories, PostWidget, PostCard, Header, Layout}
