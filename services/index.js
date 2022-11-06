a5165dc4bfb4bbb81bd7fa4d6812e0f0f4df3802
D-coder <chuksjoshuaa@gmail.com>
2022-11-06 05:50:02 +0100

    WIP: Added getRecentPosts in Services

--- services/index.js
+++ services/index.js
@@ -1,40 +1,61 @@
-import { request, gql } from "graphql-request"
+import { request, gql } from "graphql-request";
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
-
+//Get all posts
 export const getPosts = async () => {
-     
-    const query = gql`
-        query MyQuery {
-                postsConnection {
-                    edges {
-                        node {
-                            author {
-                                bio
-                                id
-                                name
-                                photo {
-                                url
-                                }
-                            }
-                            createdAt
-                            slug
-                            title
-                            excerpt
-                            featuredImage {
-                                url
-                            }
-                            categories {
-                                name
-                                slug
-                            }
-                        }
-                    }
-                }
-             }
-    `
+  const query = gql`
+    query MyQuery {
+      postsConnection {
+        edges {
+          node {
+            author {
+              bio
+              id
+              name
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
 
-    const result = await request(graphqlAPI, query)
-    return result.postsConnection.edges
-}
+  const result = await request(graphqlAPI, query);
+  return result.postsConnection.edges;
+};
+
+//Get Recent posts
+export const getRecentPosts = async () => {
+  const query = gpl`
+      query GetPostDetails(){
+        posts(
+            orderBy: createdAt_ASC
+            last: 3
+        ){
+            title
+            featuredImage {
+                url
+            }
+            createdAt
+            slug
+        }
+      }
+    `;
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+};
